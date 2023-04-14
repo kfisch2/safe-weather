799ac9cc6967dad548520a287f907b3e5474b1e2
kfisch2 <kmlindsey24@gmail.com>
2023-04-14 11:31:18 -0700

    custom designed weather icons added

--- src/components/ForecastCard.js
+++ src/components/ForecastCard.js
@@ -1,20 +1,43 @@
 import React, { useEffect } from "react";
+import snow from "../images/snow.png";
+import rain from "../images/rain.jpg";
+import clear from "../images/clear.jpg";
+import clouds from "../images/clouds.jpg";
+import lightning from "../images/lightning.jpg";
 
 export default function ForecastCard({ forecast }) {
-  console.log(forecast);
-
+  const renderWeatherIcon = (icon) => {
+    switch (icon) {
+      case "rain":
+        return <img className="weatherIcon" src={rain} />;
+      case "clouds":
+        return <img className="weatherIcon" src={clouds} />;
+      case "snow":
+        return <img className="weatherIcon" src={snow} />;
+      case "clear":
+        return <img className="weatherIcon" src={clear} />;
+      default:
+        return <img className="weatherIcon" src="" />;
+    }
+  };
   return (
     <>
       {forecast.map((day, i) => (
-        <div
-          key={i}
-          className={`forecastCard ${day.weather[0].main.toLowerCase()}`}
-        >
-          {day.weather[0].main} <div>High Temp: </div>
-          <div>Low Temp: </div>
-          <div>Humidity: </div>
-          <div>AQI: </div>
-        </div>
+        <>
+          <div
+            key={i}
+            className={`forecastCard ${day.weather[0].main.toLowerCase()}`}
+          >
+            {renderWeatherIcon(day.weather[0].main.toLowerCase())}
+            <div className="forecastInfo">
+              <div>{day.weather[0].main.toLowerCase()}</div>
+              <div>High Temp: </div>
+              <div>Low Temp: </div>
+              <div>Humidity: </div>
+              <div>AQI: </div>
+            </div>
+          </div>
+        </>
       ))}
     </>
   );
